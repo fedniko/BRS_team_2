fec4e9b1613f67857c47a5b3c5a0a42281003265
AlbertoMalvius <43897701+AlbertoMalvius@users.noreply.github.com>
2020-11-15 13:42:27 +0300

    В общем пока вот всякая мелкая хрень. Там для Main тогда я пилю лишь функции под которые есть классы?

---
+++ Crosssection.py
@@ -0,0 +1,3 @@
+class CrossSection:
+    def __init__(self, name: str):
+        self.name = name
---
+++ EducationYear.py
@@ -0,0 +1,4 @@
+class EducationYear:
+    def __init__(self, beg: int, end: int):
+        self.beginYear = beg
+        self.endYear = end
