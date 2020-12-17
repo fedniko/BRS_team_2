8611e4fcb9ef2edd51139ad03fbf22ead841c343
Helikal <ergis15@gmail.com>
2020-12-17 20:09:47 +0900

    dobavil subject, group

--- Factory.py
+++ Factory.py
@@ -1,10 +1,24 @@
 from Student import Student
+from Group import Group
+from Subject import Subject
 from datetime import date
 
 
 class Factory:
+    def __init__(self):
+        return Group(self.name, self.year)
+
+    def __init__(self):
+        return Subject(self.code, self.name)
+
     def __str__(self):
         return Student(self.сode, self.fio, self.birthdate, self.email, self.phone)
 
     def create_student(code, fio, birthdate, email, phone):
         return code.__str__(), fio.__str__(), birthdate.__str__(), email.__str__(), phone.__str__()
+
+    def create_Gruop(name, year):
+        return name.__init__(), year.__init__()
+
+    def creat_Subject(code, name):
+        return code.__init__(), name.__init__()
